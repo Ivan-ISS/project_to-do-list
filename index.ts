7ede7c893190380948e0dbe3a084938c8f78ebf5
Ivan-ISS <vanya5@inbox.ru>
2023-11-26 22:52:35 +0500

    project_to-do-list: added semantic layout

--- index.ts
+++ index.ts
@@ -1,36 +1,36 @@
 //export {};
 
 // Create a "close" button and append it to each list item
-const myNodelist: HTMLCollectionOf<Element> = document.getElementsByTagName("LI");
+const myNodelist: HTMLCollectionOf<Element> = document.getElementsByClassName('hero__list-item');
 let i: number;
 for (i = 0; i < myNodelist.length; i++) {
-    const span: HTMLElement = document.createElement("SPAN");
-    const txt: Text = document.createTextNode("\u00D7");
-    span.className = "close";
+    const span: HTMLElement = document.createElement('span');
+    const txt: Text = document.createTextNode('\u00D7');
+    span.className = 'hero__list-close';
     span.appendChild(txt);
     myNodelist[i].appendChild(span);
 }
 
 // Click on a close button to hide the current list item
-let closeButtons: HTMLCollectionOf<Element> = document.getElementsByClassName("close");
+let closeButtons: HTMLCollectionOf<Element> = document.getElementsByClassName('hero__list-close');
 for (i = 0; i < closeButtons.length; i++) {
     let closeButton: Element = closeButtons[i];
     closeButton.addEventListener('click', () => {
         const li: HTMLElement | null = closeButton.parentElement;
         if (!!li) {
-            li.style.display = "none";
+            li.style.display = 'none';
         }
     });
   }
 
 // Add a "checked" symbol when clicking on a list item
-const list: HTMLUListElement | null = document.querySelector('ul');
+const list: HTMLUListElement | null = document.querySelector('.hero__list');
 if (list) {
     list.addEventListener('click', function (ev) {
         let target: HTMLElement = <HTMLElement>ev.target;
         if (target) {
             if (target.tagName === 'LI') {
-                target.classList.toggle('checked');
+                target.classList.toggle('hero__list-item_checked');
             }
         }
     }, false);
@@ -38,38 +38,39 @@ if (list) {
 
 // Create a new list item when clicking on the "Add" button
 function newElement(): void {
-    const li: HTMLElement = document.createElement("li");
-    const input: HTMLInputElement = <HTMLInputElement>document.getElementById("myInput");
+    const li: HTMLElement = document.createElement('li');
+    li.className = 'hero__list-item';
+    const input: HTMLInputElement = <HTMLInputElement>document.getElementById('j-input');
     if (!input) return;
     const inputValue: string = input.value;
 
     const textNode: Text = document.createTextNode(inputValue);
     li.appendChild(textNode);
     if (inputValue === '') {
-        alert("You must write something!");
+        alert('You must write something!');
     } else {
-        const ul: HTMLElement | null = document.getElementById("myUL");
+        const ul: HTMLElement | null = document.getElementById('j-ul');
         if (ul) {
             ul.appendChild(li);
         }
     }
-    input.value = "";
+    input.value = '';
 
-    const span: HTMLElement = document.createElement("SPAN");
-    const txt: Text = document.createTextNode("\u00D7");
-    span.className = "close";
+    const span: HTMLElement = document.createElement('span');
+    const txt: Text = document.createTextNode('\u00D7');
+    span.className = 'hero__list-close';
     span.appendChild(txt);
     li.appendChild(span);
 
     span.addEventListener('click', () => {
         const li: HTMLElement | null = span.parentElement;
         if (!!li) {
-            li.style.display = "none";
+            li.style.display = 'none';
         }
     });
 }
 
-const addBtn: HTMLElement | null = document.getElementById('addBtn');
+const addBtn: HTMLElement | null = document.getElementById('j-add-btn');
 addBtn?.addEventListener('click', ()=> {
     newElement();
 });
